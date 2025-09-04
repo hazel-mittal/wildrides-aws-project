b300de5977d1dd12b35b71c34beb4d789a2aee26
Hazel Mittal <hmittal6@wisc.edu>
2025-09-04 13:18:28 -0700

    update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-2_fTi71IF1L', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: '2fciapvj12jgh20sstd7qcc2k4' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'us-east-2' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
